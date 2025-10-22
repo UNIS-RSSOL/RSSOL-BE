c287d55f28fdd09cb8d1b7ac874c60c5500d4d7e
LEE SIEUN <eceun@ewha.ac.kr>
2025-10-23 02:26:06 +0900

    [Feat] #9 근무표 생성 및 redis 에서 조회

--- src/main/java/com/example/unis_rssol/schedule/generation/ScheduleGenerationController.java
+++ src/main/java/com/example/unis_rssol/schedule/generation/ScheduleGenerationController.java
@@ -1,15 +1,16 @@
 package com.example.unis_rssol.schedule.generation;
 
+import com.example.unis_rssol.schedule.generation.dto.CandidateSchedule;
 import com.example.unis_rssol.schedule.generation.dto.ScheduleGenerationRequestDto;
 import com.example.unis_rssol.schedule.generation.dto.ScheduleGenerationResponseDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -24,5 +25,18 @@ public class ScheduleGenerationController {
     public ResponseEntity<ScheduleGenerationResponseDto> createSchedule(@AuthenticationPrincipal Long userId, @RequestBody ScheduleGenerationRequestDto request){
         ScheduleGenerationResponseDto response = service.createSchedules(userId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/candidates/{key}")
+    public ResponseEntity<List<CandidateSchedule>> getCandidateSchedules(@PathVariable("key") String key){
+        List<CandidateSchedule> candidates = service.getCandidateSchedules(key);
+        return ResponseEntity.ok(candidates);
+    }
+
+    @GetMapping("/redis")
+    public String redisTest() throws JsonProcessingException {
+        List<CandidateSchedule> dummy = List.of(new CandidateSchedule(1L));
+        service.testRedis(dummy);
+        return "Redis 테스트 완료";
     }
 }
--- src/main/java/com/example/unis_rssol/schedule/generation/dto/CandidateShift.java
+++ src/main/java/com/example/unis_rssol/schedule/generation/dto/CandidateShift.java
@@ -1,14 +1,14 @@
 package com.example.unis_rssol.schedule.generation.dto;
 
 import com.example.unis_rssol.schedule.DayOfWeek;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalTime;
 
+@Data
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CandidateShift {
     private Long userStoreId;
@@ -17,9 +17,19 @@ public class CandidateShift {
     private DayOfWeek day;         // 배정 요일
     private String status = null;  // "UNASSIGNED"
 
-    public CandidateShift(Long id, DayOfWeek day, LocalTime start, LocalTime end) {
+    public CandidateShift(Long userStoreId, DayOfWeek day, LocalTime start, LocalTime end) {
+        this.userStoreId = userStoreId;
+        this.day = day;
+        this.startTime = start;
+        this.endTime = end;
+        this.status = null; // 기본값
     }
 
-    public CandidateShift(Long userStoreId, DayOfWeek day, LocalTime start, LocalTime end, String unassigned) {
+    public CandidateShift(Long userStoreId, DayOfWeek day, LocalTime start, LocalTime end, String status) {
+        this.userStoreId = userStoreId;
+        this.day = day;
+        this.startTime = start;
+        this.endTime = end;
+        this.status = status;
     }
 }
