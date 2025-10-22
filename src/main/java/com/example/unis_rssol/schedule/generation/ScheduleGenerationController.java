fd0aeac8bd6b4352f59f0dc15e6b9ca857755387
LEE SIEUN <eceun@ewha.ac.kr>
2025-10-23 03:43:00 +0900

    [Feat] #9 근무표 최종 확정 및 저장

--- src/main/java/com/example/unis_rssol/schedule/generation/ScheduleGenerationController.java
+++ src/main/java/com/example/unis_rssol/schedule/generation/ScheduleGenerationController.java
@@ -1,6 +1,8 @@
 package com.example.unis_rssol.schedule.generation;
 
+import com.example.unis_rssol.schedule.entity.Schedule;
 import com.example.unis_rssol.schedule.generation.dto.CandidateSchedule;
+import com.example.unis_rssol.schedule.generation.dto.ConfirmScheduleRequestDto;
 import com.example.unis_rssol.schedule.generation.dto.ScheduleGenerationRequestDto;
 import com.example.unis_rssol.schedule.generation.dto.ScheduleGenerationResponseDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -19,19 +22,30 @@ public class ScheduleGenerationController {
     private final ScheduleGenerationService service;
 
 
-    public ScheduleGenerationController(ScheduleGenerationService service) {this.service = service;}     //주입받아서 컨트롤러 생성한다.
+    public ScheduleGenerationController(ScheduleGenerationService service) {
+        this.service = service;
+    }     //주입받아서 컨트롤러 생성한다.
 
     @PostMapping("/generate")
-    public ResponseEntity<ScheduleGenerationResponseDto> createSchedule(@AuthenticationPrincipal Long userId, @RequestBody ScheduleGenerationRequestDto request){
+    public ResponseEntity<ScheduleGenerationResponseDto> createSchedule(@AuthenticationPrincipal Long userId, @RequestBody ScheduleGenerationRequestDto request) {
         ScheduleGenerationResponseDto response = service.createSchedules(userId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/candidates/{key}")
-    public ResponseEntity<List<CandidateSchedule>> getCandidateSchedules(@PathVariable("key") String key){
+    public ResponseEntity<List<CandidateSchedule>> getCandidateSchedules(@PathVariable("key") String key) {
         List<CandidateSchedule> candidates = service.getCandidateSchedules(key);
         return ResponseEntity.ok(candidates);
     }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<?> confirmSchedule(@AuthenticationPrincipal Long userId, @RequestBody ConfirmScheduleRequestDto request) {
+        // Redis에서 후보 불러오고 startDate/endDate는 CandidateSchedule 생성 시 기록되어 있어야 함
+        Schedule finalized = service.finalizeCandidateSchedule(userId, request.getCandidateKey(), request.getStartDate(), request.getEndDate());
+
+        return ResponseEntity.ok(Map.of("status", "success", "message", "근무표 확정 완료", "scheduleId", finalized.getId()));
+    }
+
 
     @GetMapping("/redis")
     public String redisTest() throws JsonProcessingException {
