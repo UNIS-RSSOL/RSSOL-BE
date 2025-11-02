11e57c8801f156a485da77e6213b5e9a0b0ea920
LEE SIEUN <eceun@ewha.ac.kr>
2025-11-03 02:44:37 +0900

    [REFACT] 사용하지않는 구문 제거

--- src/main/java/com/example/unis_rssol/schedule/repository/WorkShiftRepository.java
+++ src/main/java/com/example/unis_rssol/schedule/repository/WorkShiftRepository.java
@@ -1,15 +1,12 @@
 package com.example.unis_rssol.schedule.repository;
 
-import com.example.unis_rssol.schedule.entity.ScheduleSettings;
 import com.example.unis_rssol.schedule.entity.WorkShift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface WorkShiftRepository  extends JpaRepository<WorkShift, Long> {//eneity, PK 형식
     List<WorkShift> findByStore_Id(Long storeId);
