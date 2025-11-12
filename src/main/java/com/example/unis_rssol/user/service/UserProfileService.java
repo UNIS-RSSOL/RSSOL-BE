c9ca20b10c47424a90b2c08cb32e1cfe7ad95db6
LEE SIEUN <eceun@ewha.ac.kr>
2025-11-12 14:38:12 +0900

    [REFACT] 주석제거

--- src/main/java/com/example/unis_rssol/user/service/UserProfileService.java
+++ src/main/java/com/example/unis_rssol/user/service/UserProfileService.java
@@ -5,7 +5,6 @@ import com.example.unis_rssol.user.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
