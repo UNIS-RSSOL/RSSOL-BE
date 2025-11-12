e9c86713d1ab0bb105b1842c91fef37345cc93f7
LEE SIEUN <eceun@ewha.ac.kr>
2025-11-12 16:16:28 +0900

    [fix] 사용하지않는 setter 제거

--- src/main/java/com/example/unis_rssol/schedule/entity/WorkShift.java
+++ src/main/java/com/example/unis_rssol/schedule/entity/WorkShift.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Entity
