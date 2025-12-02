6e1510503a930ed569169a19c693c72586ae77b3
LEE SIEUN <eceun@ewha.ac.kr>
2025-12-02 14:42:12 +0900

    [feat] swagger 토큰입력란 추가

--- src/main/java/com/example/unis_rssol/global/config/SwaggerConfig.java
+++ src/main/java/com/example/unis_rssol/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.unis_rssol.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +24,16 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Book Everywhere API")
+                        .title("RSSOL-BE API")
                         .version("v1")
-                        .description("읽는곳 API 명세서"));
+                        .description("RSSOL-BE API 명세서"))
+                // 여기에 Security Scheme 추가
+                .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
