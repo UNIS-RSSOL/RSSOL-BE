a5dcef5d7925679ceb81271a64443f4fa687acac
LEE SIEUN <eceun@ewha.ac.kr>
2026-02-18 23:48:13 +0900

    [feat] 가게 정보에 근무표생성조건 편입 (CRU) + 근무표생성 시 임시조건(REDIS) CRUD

---
+++ src/main/java/com/example/unis_rssol/domain/store/setting/SegmentDto.java
@@ -0,0 +1,17 @@
+package com.example.unis_rssol.domain.store.setting;
+
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SegmentDto {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int requiredStaff;
+}
+
